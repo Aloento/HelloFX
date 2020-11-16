c61919dbf5fc0a1c331a77a16db0365c7d22e3f0
Aloento <11802769+soarteam@users.noreply.github.com>
2020-11-16 14:46:53 +0100

    第三天

---
+++ src/main/java/com/QYun/HelloFX/第三天.java
@@ -0,0 +1,16 @@
+package com.QYun.HelloFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class 第三天 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
