2195bb5611b48bc4aa5e07790bdeeb83992991b2
Aloento <11802769+soarteam@users.noreply.github.com>
2020-11-17 13:50:13 +0100

    第四天

---
+++ src/main/java/com/QYun/HelloFX/第四天.java
@@ -0,0 +1,18 @@
+package com.QYun.HelloFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class 第四天 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+//Written by Aloento.
