34c73711cbe79165e5bca900f238a9f3708a7371
Aloento <11802769+soarteam@users.noreply.github.com>
2020-11-24 11:47:02 +0100

    第九天

--- src/main/java/com/QYun/HelloFX/第九天.java
+++ src/main/java/com/QYun/HelloFX/第九天.java
@@ -1,8 +1,17 @@
 package com.QYun.HelloFX;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class 第九天 extends Application {
@@ -13,6 +22,37 @@ public class 第九天 extends Application {
         AnchorPane aPane = new AnchorPane();
         aPane.setStyle("-fx-background-image: url('QA.jpg')");
 
+        VBox vBox = new VBox(5);
+        for (int i = 0; i < 5; i++)
+            vBox.getChildren().add(new Button("按钮" + i));
+
+        // 滚动条
+        ScrollBar sb = new ScrollBar();
+        sb.setOrientation(Orientation.VERTICAL);
+        sb.setUnitIncrement(10); // 设置按钮单次变化量
+        sb.setBlockIncrement(10); // 设置滚动条点击变化量
+        AnchorPane.setLeftAnchor(sb, 50.0);
+        sb.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                vBox.setLayoutY(-newValue.doubleValue());
+            }
+        });
+
+        HBox hBox = new HBox(5);
+        for (int i = 0; i < 10; i++)
+            hBox.getChildren().add(new Button("按钮" + i));
+        // 滚动面板
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(hBox);
+        scrollPane.setPrefWidth(200.0);
+        scrollPane.setPrefHeight(40.0);
+        AnchorPane.setTopAnchor(scrollPane, 150.0);
+
+        Separator separator = new Separator(Orientation.VERTICAL); // 灰色一条线的分离器
+        separator.setPrefHeight(150.0);
+        AnchorPane.setLeftAnchor(separator, 100.0);
+        aPane.getChildren().addAll(vBox, sb, scrollPane, separator);
         primaryStage.setScene(new Scene(aPane));
         primaryStage.setTitle("第九天");
         primaryStage.setHeight(400);
