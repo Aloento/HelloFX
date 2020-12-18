5bd632f0829eaac8f426de5fa2cc80223fce1a24
Aloento <11802769+soarteam@users.noreply.github.com>
2020-12-18 16:36:13 +0100

    第13天

--- src/main/java/com/QYun/HelloFX/第13天.java
+++ src/main/java/com/QYun/HelloFX/第13天.java
@@ -8,12 +8,14 @@ import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class 第13天 extends Application {
@@ -23,12 +25,16 @@ public class 第13天 extends Application {
 
         AnchorPane anchorPane = new AnchorPane();
         anchorPane.setStyle("-fx-background-image: url('QA.jpg')");
+
         Button b1 = new Button("单选");
         Button b2 = new Button("多选");
-        Button b3 = new Button("保存");
-        Button b4 = new Button("文件夹");
+        Button b3 = new Button("打开");
+        Button b4 = new Button("保存");
+        TextArea textArea = new TextArea();
+        textArea.setWrapText(true);
+
         VBox vBox = new VBox(5);
-        vBox.getChildren().addAll(b1, b2, b3, b4);
+        vBox.getChildren().addAll(b1, b2, b3, b4, textArea);
 
         anchorPane.getChildren().addAll(vBox);
         primaryStage.setScene(new Scene(anchorPane));
@@ -66,6 +72,64 @@ public class 第13天 extends Application {
             List<File> list = fileChooser.showOpenMultipleDialog(new Stage()); // 区别
             if (list != null) // 空安全
                 list.forEach(file -> System.out.println(file.getAbsolutePath()));
+        });
+
+        b3.setOnAction(actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("打开文本");
+            fileChooser.setInitialDirectory(new File("C:" + File.separator + "temp")); // 设置默认目录
+
+            fileChooser.getExtensionFilters().addAll( // 设定可选的类型
+                    new FileChooser.ExtensionFilter("文本类型", "*.txt", "*.docx"));
+
+            File open = fileChooser.showOpenDialog(new Stage());
+            if (open == null) // 空安全
+                return;
+            System.out.println(open.getAbsolutePath()); // 获取绝对路径
+
+            try { // 文本文档打开
+                FileInputStream fileInputStream = new FileInputStream(open); // 文件流方式改变编码GBK
+                InputStreamReader reader = new InputStreamReader(fileInputStream, "GBK");
+                // FileReader fileReader = new FileReader(open); // IO操作
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                String str;
+                while ((str = bufferedReader.readLine()) != null)
+                    textArea.appendText(str);
+
+                reader.close(); // 关闭流
+                bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        b4.setOnAction(actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("保存文本");
+            fileChooser.setInitialFileName("文本");
+            fileChooser.setInitialDirectory(new File("C:" + File.separator + "temp")); // 设置默认目录
+
+            fileChooser.getExtensionFilters().addAll( // 设定可选的类型
+                    new FileChooser.ExtensionFilter("文本类型", "*.txt", "*.docx"));
+
+            File file = fileChooser.showSaveDialog(new Stage());
+            if (file == null) // 空安全
+                return;
+            System.out.println(file.getAbsolutePath()); // 获取绝对路径
+
+            try {
+                // file.createNewFile();
+                FileOutputStream fos = new FileOutputStream(file); // 文件流
+                OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8); // 输出流
+                osw.write(textArea.getText());  // 获得内容
+
+                osw.close(); // 流关闭顺序重要
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         });
 
     }
