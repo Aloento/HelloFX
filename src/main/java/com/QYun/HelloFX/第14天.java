64aa5dff6195f10fa2ac66c6bf53a3f6f27886cb
Aloento <11802769+soarteam@users.noreply.github.com>
2020-12-22 14:16:45 +0100

    第14天

---
+++ src/main/java/com/QYun/HelloFX/第14天.java
@@ -0,0 +1,17 @@
+package com.QYun.HelloFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class 第14天 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
